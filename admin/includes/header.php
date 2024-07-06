250a98de7c3676508cc6f0af4f9167d2d0b9d1cd
Cococips <121195750+Cococip@users.noreply.github.com>
2024-07-06 16:47:48 +0700

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="dicoding:email" content="riskadesti123@gmail.com">
   <link href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css" rel="stylesheet" />
   <link rel="icon" href="includes/assets/img/logo.png" type="icon" />
   <link rel="stylesheet" href="includes/assets/css/styles.css" />
