34e1c969b518f58d41202067ab670b6d3b570b8e
Cococips <121195750+Cococip@users.noreply.github.com>
2024-07-06 16:48:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="dicoding:email" content="riskadesti123@gmail.com"/>
     <link rel="icon" type="image/png" href="public/images/logo/logo-arvene-ver.png">
     <link rel="stylesheet" href="public/css/main.css">
 
